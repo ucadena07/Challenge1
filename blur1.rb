45df84ce2f4f3243dd1c0d50a390f6717487ef37
Ulises Cadena <ucadena07@gmail.com>
2019-09-30 17:40:02 +0000

    initial commit

---
+++ blur1.rb
@@ -0,0 +1,20 @@
+
+class Image 
+  def initialize(picture)
+    @picture = picture
+  end
+
+  def output_image
+    @picture.each do |pixel|
+      puts pixel.join
+      
+    end
+  end
+
+
+end
+
+
+image = Image.new([[0, 0, 0, 0],[0, 1, 0, 0],[0, 0, 0, 1],[0, 0, 0, 0]
+])
+image.output_image
